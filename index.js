5e1adcb4936bebc1a5a1de9be1f1fec32d44cb71
Vu Huy <vuhuycto@gmail.com>
2020-08-02 20:52:08 +0700

    temporary comment out logging

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const app = express();
 
 require('./startup/config')();
 require('./startup/db')();
-require('./startup/logging')();
+// require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/validation')();
 require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
-	winston.info(`Listening to port ${port}`);
+	// winston.info(`Listening to port ${port}`);
 });
 
 module.exports = server;

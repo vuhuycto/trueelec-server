106ba0622c66209e23040b7bece029d260510943
Vu Huy <vuhuycto@gmail.com>
2020-08-04 15:39:57 +0700

    comment out logging

--- startup/logging.js
+++ startup/logging.js
@@ -4,18 +4,16 @@ require('winston-mongodb');
 require('express-async-errors');
 
 module.exports = function () {
-	process.on('unhandledRejection', (ex) => {
-		throw ex;
-	});
-
-	winston.handleExceptions(
-		new winston.transports.Console({ colorize: true, prettyPrint: true }),
-		new winston.transports.File({ filename: 'uncaughtExceptions.log' })
-	);
-
-	winston.add(winston.transports.File, { filename: 'logfile.log' });
-	winston.add(winston.transports.MongoDB, {
-		db: config.get('db'),
-		level: 'error',
-	});
+	// process.on('unhandledRejection', (ex) => {
+	// 	throw ex;
+	// });
+	// winston.handleExceptions(
+	// 	new winston.transports.Console({ colorize: true, prettyPrint: true }),
+	// 	new winston.transports.File({ filename: 'uncaughtExceptions.log' })
+	// );
+	// winston.add(winston.transports.File, { filename: 'logfile.log' });
+	// winston.add(winston.transports.MongoDB, {
+	// 	db: config.get('db'),
+	// 	level: 'error',
+	// });
 };
